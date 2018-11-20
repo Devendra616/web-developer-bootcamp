a99ffd9c0b2ce23f137d2eb50107c32f0b7458f8
Devendra616 <devendra.616@gmail.com>
2018-11-20 10:52:51 +0530

    jQuery bootcamp excercise

---
+++ jQueryExercise/exercise.js
@@ -0,0 +1,10 @@
+// Select all divs and give them a purple background
+$('div').css("backgroundColor","purple");
+
+// Select the divs with class "highlight" and make them 200px wide
+$('.highlight').css("width","200px");
+// Select the div with id "third" and give it a orange border
+$("#third").css("border","3px solid orange");
+
+// Bonus: Select the first div only and change its font color to pin
+$("div:first").css("color","pink");
